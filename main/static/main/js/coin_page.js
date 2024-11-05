53dbebbd8509ef9cd531863bab54b0680b1cc484
Ruslan <136484210+ober0@users.noreply.github.com>
2024-11-05 17:55:59 +0300

    предсказание работает

--- main/static/main/js/coin_page.js
+++ main/static/main/js/coin_page.js
@@ -2,28 +2,34 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('.btn').forEach(btn => {
         btn.addEventListener('click', function () {
             const timeArg = btn.getAttribute('arg');
+            let coin_name = document.getElementById('coin_name').innerText
             if (timeArg === '1d') {
-                window.location.href = 'http://localhost:8000/coin/NEAR/?time=1d';
+                window.location.href = `http://localhost:8000/coin/${coin_name}/?time=1d`;
             }
             if (timeArg === '7d') {
-                window.location.href = 'http://localhost:8000/coin/NEAR/?time=7d';
+                window.location.href = `http://localhost:8000/coin/${coin_name}/?time=7d`;
             }
             if (timeArg === '20d') {
-                window.location.href = 'http://localhost:8000/coin/NEAR/?time=20d';
+                window.location.href = `http://localhost:8000/coin/${coin_name}/?time=20d`;
             }
         });
     });
 
     document.getElementById('go_predict').addEventListener('click', function () {
+        document.getElementById('predict_col').innerHTML = 'Загрузка...'
         const task_id = document.getElementById('task_id').value;
-
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        let data = {
+            'task_id': task_id
+        }
         const intervalId = setInterval(() => {
-            fetch(`api/result/${task_id}`, {
+            fetch(`/api/result/${task_id}/`, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken
                 },
-                body: JSON.stringify({ task_id })
+                body: JSON.stringify(data)
             })
             .then(response => response.json())
             .then(data => {
